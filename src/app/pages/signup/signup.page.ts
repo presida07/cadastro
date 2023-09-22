cafef0740c4cec705a8be54aef8d0d15dc18467f
presida07 <duduchimenas@gmail.com>
2023-09-22 11:53:45 -0300

    55m

--- src/app/pages/signup/signup.page.ts
+++ src/app/pages/signup/signup.page.ts
@@ -1,7 +1,8 @@
 import { AuthentivationService } from './../../authentivation.service';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { LoadingController } from '@ionic/angular/providers/loading-controller';
+import { LoadingController } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-signup',
@@ -15,8 +16,8 @@ export class SignupPage implements OnInit {
 
   ngOnInit() {
     this.regForm = this.formBuilder.group({
-      fullname :['', [Validators.required]],
-      email :['',[
+      fullname:['', [Validators.required]],
+      email:['',[
         Validators.required,
         Validators.pattern("[a-z0-9._%+\-]+@[a-z0-9.\-]+\.[a-z]{2,}$"),
         Validators.email,
